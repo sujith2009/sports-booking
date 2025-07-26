7986dfc30038c217988a0c69905b385c3f31c3ba
sujith2009 <jerinsujith640@gmail.com>
2025-07-26 08:09:59 +0530

    auth

--- context/Authcontext.jsx
+++ context/Authcontext.jsx
@@ -16,6 +16,7 @@ export const AuthProvider = ({ children }) => {
     }
   }, []);
 
+  //auth provices
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       {children}
